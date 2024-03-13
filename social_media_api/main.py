b1f88a8492e08d653ae91bf33256ebe3f5a5366f
Cauê Garcia Polimanti <caue.polimanti@gmail.com>
2024-03-13 16:01:42 -0300

    feat: setting up simple logging

--- social_media_api/main.py
+++ social_media_api/main.py
@@ -1,12 +1,19 @@
+import logging
+
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from social_media_api.database import database
+from social_media_api.logging_conf import configure_logging
 from social_media_api.routers.post import router as post_router
 
 
+logger = logging.getLogger(__name__)
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    configure_logging()
+    logger.info("Logging configured")
     await database.connect()
     yield
     await database.disconnect()
